b7adf9fee8c84a3dee66cfe1adbfa7ec40b20e86
Yariazen <forschool644@gmail.com>
2022-08-24 22:04:02 -0700

    save

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,55 +1,40 @@
 local S = "__space-factorissimo-updated__"
 
-data:extend {
-    {
-		type = "technology",
-		name = "space-factory-architecture-t1",
-		icon = S.."/graphics/technology/space-factory-architecture-1.png",
-		icon_size = 128,
-		prerequisites = {"factory-architecture-t3", "se-space-platform-plating", "electric-energy-distribution-2"},
-		effects = {
-			{type = "unlock-recipe",recipe = "space-factory-1"}
-		},
-		unit = {
-			count = 5000,
-			ingredients = {
-				{"automation-science-pack", 1},
-				{"logistic-science-pack", 1},
-				{"chemical-science-pack", 1},
-				{"se-rocket-science-pack", 1},
-				{"space-science-pack", 1}, 
-				{"production-science-pack", 1}
-			},
-			time = 60
-		},
-		order = "a-c"
-	},
-	{
-		type = "technology",
-		name = "space-gravFactory-architecture",
-		icon = S.."/graphics/technology/space-gravFactory-architecture.png",
-		icon_size = 128,
-		prerequisites = {"factory-architecture-t3", "se-space-platform-plating", "se-deep-space-science-pack-1"},
-		effects = {
-			{type = "unlock-recipe",recipe = "space-gravFactory"}
-		},
-		unit = {
-			count = 5000,
-			ingredients = {
-				{"automation-science-pack", 1},
-				{"logistic-science-pack", 1},
-				{"chemical-science-pack", 1},
-				{"se-rocket-science-pack", 1},
-				{"space-science-pack", 1},
-				{"utility-science-pack", 1},
-				{"production-science-pack", 1},
-				{"se-astronomic-science-pack-1", 1},
-				{"se-energy-science-pack-1", 1},
-				{"se-material-science-pack-1", 1},
-				{"se-deep-space-science-pack-1", 1}
-			},
-			time = 60
-		},
-		order = "a-c"
+local function tier_1_architecture(ingredients)
+	return {
+		count = 5000,
+		ingredients = ingredients,
+		time = 60
 	}
-}
+end
+
+function tier_2_architecture(ingredients)
+	return {
+		count = 5000,
+		ingredients = ingredients,
+		time = 60
+	}
+end
+
+function tier_3_architecture(ingredients)
+	return {
+		count = 5000,
+		ingredients = ingredients,
+		time = 60
+	}
+end
+
+function technology_prototype(name, prerequisites, effects, unit)
+	data:extend {
+		{
+			type = "technology",
+			name = name,
+			icon = S .. "/graphics/technology/" .. ".png",
+			icon_size = 128,
+			prerequisites = prerequisites,
+			effects = effects,
+			unit = unit,
+			order = "a-c"
+		}
+	}
+end
